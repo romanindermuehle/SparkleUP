20493d8da46bb88bade18321cfb08fb7762fdd59
Roman Indermühle <roman.indermuehle@icloud.com>
2024-05-12 14:52:26 +0200

    Array of greetings added

---
+++ SparkleUP/Common/Data/Greetings.swift
@@ -0,0 +1,32 @@
+//
+//  Greetings.swift
+//  SparkleUP
+//
+//  Created by Roman Indermühle on 05.05.2024.
+//
+
+import Foundation
+
+struct Greetings {
+    static var messages: [String] = [
+        "Faith Trust Believe",
+        "Let It Sparkle",
+        "Embrace Your Shine",
+        "Unlock Your Potential",
+        "Ignite Your Spark",
+        "Fuel Your Passion",
+        "Craft Your Bliss",
+        "Find Your Flow",
+        "Celebrate Your Wins",
+        "Cultivate Your Gratitude",
+        "Nurture Your Dreams",
+        "Shine Bright Today",
+        "Elevate Your Spirit",
+        "Be Your Best Self",
+        "Rise and Shine",
+        "Chase Your Dreams",
+        "Transform Your Reality",
+        "Craft Your Destiny",
+        "Radiate Inner Peace"
+    ]
+}
