ded979078439ba85a1ae3316d447671a9eadff26
Roman Indermühle <roman.indermuehle@icloud.com>
2024-05-12 17:52:07 +0200

    View Overhaul

--- SparkleUP/App/Quote/QuoteAddView.swift
+++ SparkleUP/App/Quote/QuoteAddView.swift
@@ -20,51 +20,67 @@ struct QuoteAddView: View {
     
     
     var body: some View {
-        List {
-            Section {
-                TipView(quoteTip, arrowEdge: .bottom)
-                    .padding()
-                    #if os(iOS)
-                    .tipBackground(Color.accentColor.opacity(0.1))
-                    #endif
-            }
-            .listRowBackground(Color.clear)
-            .listRowSeparator(.hidden)
-            
-            ForEach(quotes) { quote in
-                VStack(alignment: .leading) {
-                    HStack {
-                        Button {
-                            animationToggled.toggle()
-                            quote.isFavorite.toggle()
-                        } label: {
-                            Image(systemName: quote.isFavorite ? "star.fill" : "star.slash.fill")
-                                .font(.title)
-                                .foregroundStyle(quote.isFavorite ? Color.accentColor : Color.gray)
+        VStack {
+            if quotes.isEmpty {
+                ContentUnavailableView {
+                    Label("No quotes loaded yet", systemImage: "icloud.and.arrow.down")
+                } description: {
+                    Text("First, tap the \"Load Quotes\" button to load all suggested quotes.")
+                } actions: {
+                    Button() {
+                        for quote in QuoteSampleData.quotes {
+                            context.insert(quote)
                         }
-                        .buttonStyle(.borderless)
-                        .symbolEffect(.bounce, value: animationToggled)
                         
-                        VStack(alignment: .leading) {
-                            Text(quote.quote)
-                            Text(quote.author)
-                        }
+                    } label: {
+                        Text("Load Quotes")
+                            .padding(5)
+                            .font(.headline)
                     }
-                    .swipeActions {
-                        Button {
-                            quote.isFavorite.toggle()
-                        } label: {
-                            Image(systemName: quote.isFavorite ? "star.slash.fill" : "star.fill")
-                                .font(.title)
-                                .tint(quote.isFavorite ? Color.gray : Color.accentColor)
+                    .buttonStyle(.borderedProminent)
+                }
+            } else {
+                VStack {
+                    TipView(quoteTip, arrowEdge: .bottom)
+                        .padding()
+                    List {
+                        ForEach(quotes) { quote in
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Button {
+                                        animationToggled.toggle()
+                                        quote.isFavorite.toggle()
+                                    } label: {
+                                        Image(systemName: quote.isFavorite ? "star.fill" : "star.slash.fill")
+                                            .font(.title)
+                                            .foregroundStyle(quote.isFavorite ? .accent : .gray)
+                                    }
+                                    .buttonStyle(.borderless)
+                                    .symbolEffect(.bounce, value: animationToggled)
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text(quote.quote)
+                                        Text(quote.author)
+                                    }
+                                }
+                                .swipeActions {
+                                    Button {
+                                        quote.isFavorite.toggle()
+                                    } label: {
+                                        Image(systemName: quote.isFavorite ? "star.slash.fill" : "star.fill")
+                                            .font(.title)
+                                            .tint(quote.isFavorite ? .gray : .accent)
+                                    }
+                                    
+                                }
+                            }
                         }
-                        
+                     
                     }
                 }
             }
         }
-        
-        .navigationTitle("Add Quote")
+        .navigationTitle("Choose Quotes")
         .navigationBarTitleDisplayMode(.inline)
         .scrollIndicators(.hidden)
         .toolbar {
@@ -73,13 +89,6 @@ struct QuoteAddView: View {
                     dismiss()
                 }
                 .fontWeight(.bold)
-            }
-        }
-        .onAppear {
-            if quotes.isEmpty {
-                for quote in SampleData.quotes {
-                    context.insert(quote)
-                }
             }
         }
     }
--- SparkleUP/App/Today/TodayView.swift
+++ SparkleUP/App/Today/TodayView.swift
@@ -7,39 +7,51 @@
 
 import SwiftUI
 import SwiftData
-import Charts
 import TipKit
 
 struct TodayView: View {
-    @Query var users: [User]
-    @Query var days: [Day]
+    @Query(sort: \Day.startedAt) var days: [Day]
     @Environment(\.modelContext) var context
     
     @State var showSparkle: Bool = false
     
+    @State private var currentGreeting: String = ""
+    
     var ringTip = RingTip()
+    
+    var splitViewVisibility: NavigationSplitViewVisibility?
+    
+    var screenWidth: Int? {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            return Int(windowScene.coordinateSpace.bounds.width) - (splitViewVisibility == .detailOnly ? 0: 100)
+        }
+        return nil
+    }
+    
+    var screenHeight: Int? {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            return Int(windowScene.coordinateSpace.bounds.height) / (splitViewVisibility == .detailOnly ? 0: 3)
+        }
+        return nil
+    }
     
     var body: some View {
         NavigationStack {
             if let day = days.last {
                 Form {
                     Section {
-                        ProgressRing(day: day, ringSizeHeight: 350, ringSizeWidth: 350, ringThickness: 30.0, ringHeight: 30.0, ringWidth: 30.0, fontSize: 64)
+                        ProgressRing(day: day, ringSizeHeight: CGFloat((screenHeight ?? 0)), ringSizeWidth: CGFloat(screenWidth ?? 0), ringThickness: 30.0, ringHeight: 30.0, ringWidth: 30.0, fontSize: 58)
                             .padding(.top, 8)
                             .padding(.bottom, 35)
                         
-                        
                         TipView(ringTip, arrowEdge: .top)
-                            .padding()
-                        #if os(iOS)
-                            .tipBackground(Color.accentColor.opacity(0.1))
-                        #endif
-                        
+#if(!os(visionOS))
+                            .tipBackground(.lightMagenta.opacity(0.2))
+#endif
                     }
                     .listRowBackground(Color.clear)
                     .listRowSeparator(.hidden)
                     .frame(maxWidth: .infinity, alignment: .center)
-                    
                     
                     Section("Daily tasks") {
                         ForEach(DailyTask.allCases, id: \.rawValue) { dailyTask in
@@ -53,17 +65,24 @@ struct TodayView: View {
                         }
                     }
                 }
-                .navigationTitle("Hello, \(users.first?.name ?? "User")")
+                .navigationTitle(currentGreeting)
                 .onAppear {
                     
-                    if day.percentage >= 1.0 && day.sparkleSeen != true {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000)) {
-                            showSparkle.toggle()
-                        }
+                    if day.percentage >= 1.0 && day.sparkleSeen == false {
+                        showSparkle.toggle()
                         day.sparkleSeen = true
                     }
                     
-                    checkDayOver(startedAt: day.startedAt)
+                    selectNewGreeting()
+                    
+                    let daysToInsert = checkDayOver(startedAt: day.startedAt)
+                    
+                    guard let current = Calendar.current.date(byAdding: .day, value: 1, to: Date()) else {return}
+                    
+                    let futureDay = [Day(startedAt: current)]
+                            
+                    
+                    createDay(days: futureDay)
                 }
                 .navigationDestination(for: DailyTask.self) { dailyTask in
                     dailyTask.destination
@@ -77,26 +96,47 @@ struct TodayView: View {
         }
     }
     
-    func checkDayOver(startedAt: Date) {
-        let currentDate = Date()
-        
-        let isTomorrow = Calendar.current.isDateInTomorrow(startedAt)
-        guard let dayDiffernce = Calendar.current.dateComponents([.day], from: currentDate, to: startedAt).day else { return }
-        
-        if isTomorrow {
-            createDay()
-        } else if dayDiffernce >= 2 {
-            print(dayDiffernce)
-            for _ in (0...dayDiffernce) {
-                createDay()
-            }
-        } else {
-            return
-        }
+    func selectNewGreeting() {
+        Greetings.messages.shuffle()
+        currentGreeting = Greetings.messages.randomElement() ?? ""
     }
     
-    func createDay() {
-        context.insert(Day.init())
+    func checkDayOver(startedAt: Date, current: Date = .now) -> [Day] {
+        var days: [Day] = []
+        
+        let isNotToday = !Calendar.current.isDateInToday(startedAt)
+        
+        if let dayDifference = calculateDayDifference(fromDate: startedAt, toDate: current) {
+            if dayDifference >= 2 {
+                for n in 0..<dayDifference {
+                    if let daySubtractedDate = subtractDayFromDate(numberOfDays: n, subtractFrom: current) {
+                        let insertDay = Day(startedAt: daySubtractedDate)
+                        days.append(insertDay)
+                    }
+                    
+                }
+            } else if isNotToday {
+                let newDay = Day(startedAt: current)
+                days.append(newDay)
+            }
+        }
+        
+        return days
+    }
+    
+    func calculateDayDifference(fromDate: Date, toDate: Date) -> Int? {
+        Calendar.current.dateComponents([.day], from: fromDate, to: toDate).day
+    }
+    
+    func subtractDayFromDate(numberOfDays: Int, subtractFrom: Date) -> Date? {
+        Calendar.current.date(byAdding: .day, value: -numberOfDays, to: subtractFrom)
+    }
+    
+    func createDay(days: [Day]) {
+        for day in days {
+            context.insert(day)
+        }
+        
     }
 }
 
