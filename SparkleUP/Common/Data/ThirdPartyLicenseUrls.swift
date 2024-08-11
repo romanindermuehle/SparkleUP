7302c2238b64fa940e15b2186db99980f1c30569
Roman Indermühle <roman.indermuehle@icloud.com>
2024-08-11 17:03:25 +0200

    Array containing third party licensed URLs added

---
+++ SparkleUP/Common/Data/ThirdPartyLicenseUrls.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdPartyLicenseUrls.swift
+//  SparkleUP
+//
+//  Created by Roman Indermühle on 11.08.2024.
+//
+
+import Foundation
+
+struct ThirdPartyLicenseUrl {
+    let id = UUID()
+    var name: String
+    var url: URL?
+}
+
+struct ThirdPartyLicenseUrls {
+    static let urls: [ThirdPartyLicenseUrl] = [
+        ThirdPartyLicenseUrl(name: "Vortex(MIT Licence)", url: URL(string: "https://github.com/twostraws/Vortex?tab=MIT-1-ov-file")), ThirdPartyLicenseUrl(name: "Unsplash Licence", url: URL(string: "https://unsplash.com/de/lizenz"))
+    ]
+}
