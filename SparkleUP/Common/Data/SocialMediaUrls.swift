59856cd83fe085801d4dde1f7058b38d6ef39d8b
Roman Indermühle <roman.indermuehle@icloud.com>
2024-08-11 17:05:08 +0200

    Created struct for social media url properties

---
+++ SparkleUP/Common/Data/SocialMediaUrls.swift
@@ -0,0 +1,19 @@
+//
+//  SocialMediaUrls.swift
+//  SparkleUP
+//
+//  Created by Roman Indermühle on 11.08.2024.
+//
+
+import Foundation
+
+struct SocialMediaUrls {
+    static let mastodonURL: URL? = URL(string: "https://mastodon.social/@iCraftedSolutions")
+    static let twitterURL: URL? = URL(string: "https://twitter.com/IndermuhleRoman")
+    static let linkedInRIURL: URL? = URL(string: "https://www.linkedin.com/in/roman-indermühle-83b297249/")
+    static let websiteRIURL: URL? = URL(string: "https://www.romanindermuehle.ch")
+    static let linkedInCFURL: URL? = URL(string: "http://www.linkedin.com/in/corinnefurch-business-mentoring")
+    static let facebookURL: URL? = URL(string: "https://m.facebook.com/groups/diamondmindsetacademy")
+    static let instagramURL: URL? = URL(string: "https://www.instagram.com/corinnefurch")
+    static let websiteCFURL: URL? = URL(string: "https://www.corinnefurch.com")
+}
