49320120b1ac3e9521ce91670e774d10a88d290e
Roman Indermühle <roman.indermuehle@icloud.com>
2024-08-11 20:23:49 +0200

    Removed unnecessary tests

--- SparkleUPTests/TodayViewTests.swift
+++ SparkleUPTests/TodayViewTests.swift
@@ -10,84 +10,22 @@ import XCTest
 
 final class TodayViewTests: XCTestCase {
 
-    func testOneDayDiffernce() {
-        guard let currentDate = Date.createSpecificDate(year: 2024, month: 5, day: 10) else { return }
-        guard let startedAt = Date.createSpecificDate(year: 2024, month: 5, day: 9) else { return }
+    func testNewDaySuccessful() {
+        let days: [Day] = [Day(startedAt: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date()), Day(startedAt: Date.now)]
         let todayView = TodayView()
         
-        let days = todayView.checkDayOver(startedAt: startedAt, current: currentDate)
+        let newDay = todayView.createNewDay(days: days)
 
-        XCTAssertEqual(days.count, 1)
-        XCTAssertEqual(days.first?.startedAt.formatted(date: .numeric, time: .omitted), "5/10/2024")
+        XCTAssertNil(newDay)
     }
     
-    func testFiveDayDiffernce() {
-        guard let startedAt = Date.createSpecificDate(year: 2024, month: 5, day: 5) else { return }
-        guard let currentDate = Date.createSpecificDate(year: 2024, month: 5, day: 10) else { return }
-        
+    func testNewDayNegative() {
+        let days: [Day] = [Day(startedAt: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date())]
         let todayView = TodayView()
         
-        let days = todayView.checkDayOver(startedAt: startedAt, current: currentDate)
-        
-        XCTAssertEqual(days.count, 5)
-        XCTAssertFalse(days.last?.startedAt.formatted(date: .numeric, time: .omitted) == "5/5/2024")
-        XCTAssertTrue(days.last?.startedAt.formatted(date: .numeric, time: .omitted) == "5/6/2024")
-    }
-    
-    func testFiveDaysInFutureDiffernce() {
-        guard let currentDate = Date.createSpecificDate(year: 2024, month: 5, day: 10) else { return }
-        guard let startedAt = Date.createSpecificDate(year: 2024, month: 5, day: 15) else { return }
-       
-        let todayView = TodayView()
-        
-        let days = todayView.checkDayOver(startedAt: startedAt, current: currentDate)
-        
-        XCTAssertEqual(days.count, 1)
-        XCTAssertFalse(days.last?.startedAt.formatted(date: .complete, time: .omitted) == "5/5/2024")
-        XCTAssertTrue(days.last?.startedAt.formatted(date: .numeric, time: .omitted) == "5/10/2024")
-    }
-    
-    func testSuccessfulCalculateDayDifference() {
-        guard let currentDate = Date.createSpecificDate(year: 2024, month: 5, day: 10) else { return }
-        guard let startedAt = Date.createSpecificDate(year: 2024, month: 5, day: 5) else { return }
-       
-        let todayView = TodayView()
-        
-        let numberOfDays = todayView.calculateDayDifference(fromDate: startedAt, toDate: currentDate)
-        
-       XCTAssertEqual(numberOfDays, 5)
-    }
-    
-    func testNegativeCalculateDayDifference() {
-        guard let currentDate = Date.createSpecificDate(year: 2024, month: 5, day: 5) else { return }
-        guard let startedAt = Date.createSpecificDate(year: 2024, month: 5, day: 10) else { return }
-       
-        let todayView = TodayView()
-        
-        let numberOfDays = todayView.calculateDayDifference(fromDate: startedAt, toDate: currentDate)
-        
-       XCTAssertEqual(numberOfDays, -5)
-    }
-    
-    func testSuccessfulSubtractDayFromDate() {
-        guard let currentDate = Date.createSpecificDate(year: 2024, month: 5, day: 10) else { return }
-        let numberOfDays = 5
-       
-        let todayView = TodayView()
-        
-        let subtractedDate = todayView.subtractDayFromDate(numberOfDays: numberOfDays, subtractFrom: currentDate)
-        
-        XCTAssertEqual(subtractedDate?.formatted(date: .numeric, time: .omitted), "5/5/2024")
-    }
-    
-    func testNegativeSubtractDayFromDate() {
-        guard let currentDate = Date.createSpecificDate(year: 2024, month: 5, day: 10) else { return }
-        let numberOfDays = -5
-       
-        let todayView = TodayView()
-        
-        let subtractedDate = todayView.subtractDayFromDate(numberOfDays: numberOfDays, subtractFrom: currentDate)
-        
-        XCTAssertFalse(subtractedDate?.formatted(date: .numeric, time: .omitted) != "5/15/2024")
+        let newDay = todayView.createNewDay(days: days)
+
+        XCTAssertEqual(newDay?.day.startedAt.formatted(date: .abbreviated, time: .omitted), Date().formatted(date: .abbreviated, time: .omitted))
+        XCTAssertEqual(newDay?.streak.addedAt.formatted(date: .abbreviated, time: .omitted), Date().formatted(date: .abbreviated, time: .omitted))
     }
 }
