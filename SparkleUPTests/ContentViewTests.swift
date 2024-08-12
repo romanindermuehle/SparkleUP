baf1c5c9a18780cf4e6b0561e508e75013e2fa8b
Roman Indermühle <roman.indermuehle@icloud.com>
2024-08-12 08:09:15 +0200

    Renamed TodayViewTests to ContentViewTests

--- SparkleUPTests/ContentViewTests.swift
+++ SparkleUPTests/ContentViewTests.swift
@@ -8,22 +8,22 @@
 import XCTest
 @testable import SparkleUP
 
-final class TodayViewTests: XCTestCase {
+final class ContentViewTests: XCTestCase {
 
     func testNewDaySuccessful() {
         let days: [Day] = [Day(startedAt: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date()), Day(startedAt: Date.now)]
-        let todayView = TodayView()
+        let contentView = ContentView()
         
-        let newDay = todayView.createNewDay(days: days)
+        let newDay = contentView.createNewDay(days: days)
 
         XCTAssertNil(newDay)
     }
     
     func testNewDayNegative() {
         let days: [Day] = [Day(startedAt: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date())]
-        let todayView = TodayView()
+        let contentView = ContentView()
         
-        let newDay = todayView.createNewDay(days: days)
+        let newDay = contentView.createNewDay(days: days)
 
         XCTAssertEqual(newDay?.day.startedAt.formatted(date: .abbreviated, time: .omitted), Date().formatted(date: .abbreviated, time: .omitted))
         XCTAssertEqual(newDay?.streak.addedAt.formatted(date: .abbreviated, time: .omitted), Date().formatted(date: .abbreviated, time: .omitted))
